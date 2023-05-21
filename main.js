f3e57eee91c083e136d1ee0052a8f0a0d43c5aa4
Janaina Tavares <112178884+profJana@users.noreply.github.com>
2023-05-21 19:42:23 -0300

    Projeto atualizado

--- main.js
+++ main.js
@@ -1,11 +1,48 @@
 var modal = document.getElementById("modalCapuccino");
+var modalTorta = document.getElementById("modalTorta");
+var modalSalgados = document.getElementById("modalSalgados");
+var modalMilshakes = document.getElementById("modalMilkshakes");
 var abrirModal = document.getElementById("openModal");
+var abrirSalgados = document.getElementById("openSalgados");
+var abrirMilkshakes = document.getElementById("openMilkshake");
+var abrirTorta = document.getElementById("openTorta");
 var fecharModal = document.getElementsByClassName("close")[0];
+var closeTortas = document.getElementsByClassName("closeTortas")[0];
+var closeSalgados = document.getElementsByClassName("closeSalgados")[0];
+var closeMilkshakes = document.getElementsByClassName("closeMilkshake")[0];
 
+//café
 abrirModal.onclick = function(){
     modal.style.display = "block";
 }
-
 fecharModal.onclick = function(){
     modal.style.display = "none";
 }
+
+
+//salgados
+abrirSalgados.onclick = function(){
+    modalSalgados.style.display = "block"; //visível
+}
+closeSalgados.onclick = function(){
+    modalSalgados.style.display = "none"; //invisível
+}
+
+//torta
+abrirTorta.onclick = function(){
+    modalTorta.style.display = "block";
+}
+closeTortas.onclick = function(){
+    modalTorta.style.display = "none";
+}
+
+//milkshake
+abrirMilkshakes.onclick = function(){
+    modalMilshakes.style.display = "block";
+}
+closeMilkshakes.onclick = function(){
+    modalMilshakes.style.display = "none";
+}
+
+
+
